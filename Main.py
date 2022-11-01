9cef0de96024c31c8665ed67c61fcce3f8e40712
URK21CS2064 <109648000+URK21CS2064@users.noreply.github.com>
2022-11-01 14:22:06 +0530

    completed

--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
 class Evaluate:
   """This class validates and evaluate postfix expression.
   Attributes:
-      top: An integer which denotes the index of the element at the top of the stack currently.
+      top: An integer which denotes the index of the element at the top of the stack currently
       size_of_stack: An integer which represents the size of stack.
       stack: A List which acts as a Stack.
   """
